ccd40570898c724074d60f085197f3ad75fe6784
lucioperez01 <perezluciogabriel@gmail.com>
2026-08-27 13:38:38 -0300

    fixing dynamic rendering

--- app/(dashboard)/dashboard/products/page.tsx
+++ app/(dashboard)/dashboard/products/page.tsx
@@ -4,7 +4,7 @@ import { CategoryDTO } from "@/Interfaces/dto/product.dto"
 import { getCategories } from "@/application/use-cases/get-categories"
 import { getProducts } from "@/application/use-cases/get-products"
 import ProductsClient from "./ProductClient"
-
+export const dynamic = 'force-dynamic';
 export default async function ProductPage() {
     const categories: CategoryDTO[] = await getCategories();
     const products: ProductDTO[] = await getProducts();
